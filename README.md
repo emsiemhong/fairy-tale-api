4cedf0ecd16edbe6114ab42e423a17e596bfa8e5
siemhong <emsiemhong666@gmail.com>
2023-12-05 11:47:33 +0700

    Initialize Fairy tale api

---
+++ README.md
@@ -0,0 +1,63 @@
+# Fairy Tale Stories API
+
+This API allows you to manage and retrieve fairy tale stories. Below are the available endpoints and their functionalities.
+
+## Endpoints
+
+### Get All Fairy Tales
+
+```
+GET /api/fairytales
+```
+
+Returns a list of all fairy tales.
+
+### Get a Single Fairy Tale
+
+```
+GET /api/fairytales/{id}
+```
+Returns a single fairy tale by ID.
+
+### Filter Fairy Tales by Age
+
+```
+GET /api/fairytales/filterByAge/{age}
+```
+
+Returns a filtered list of fairy tales by age range. The age parameter can take values like "3-7", "2-8", or "All".
+
+### Sort Fairy Tales by Title(order: 'asc' or 'desc')
+
+```
+GET /api/fairytales/sortByTitle/{order}
+```
+
+Returns a sorted list of fairy tales by title (ascending or descending).
+
+### Sort Fairy Tales by Created Date
+
+```
+GET /api/fairytales/sortByCreatedAt/{order}
+```
+Returns a sorted list of fairy tales by created date (ascending or descending).
+
+### Usage
+
+Make requests to the respective endpoints using your preferred HTTP client.
+
+### Response Format
+
+The API responses will be in JSON format and follow this structure:
+```
+{
+  "id": "0008",
+  "title": "The Little Mermaid",
+  "author": "Hans Christian Andersen",
+  "created_at": "1837-01-01T00:00:00.000Z",
+  "image": "./images/The Little Mermaid.jpeg",
+  "audio": "./audios/កនជរកទង  Three Little Pigs in Khmer   Khmer Fairy Tales.mp3",
+  "age": "All",
+  "summary": "The Little Mermaid sacrifices her voice for love, longing to join the human world."
+}
+```
---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const fairyTales = require("./fairyTales.json"); // Assuming fairyTales.json contains your data
+
+// Route to get all fairy tales
+app.get("/api/fairytales", (req, res) => {
+  res.json(fairyTales);
+});
+
+// Route to get a single fairy tale by ID
+app.get("/api/fairytales/:id", (req, res) => {
+  const id = req.params.id;
+  const tale = fairyTales.find((tale) => tale.id === id);
+  if (!tale) {
+    res.status(404).json({ message: "Fairy tale not found" });
+  } else {
+    res.json(tale);
+  }
+});
+
+// Route to filter fairy tales by age
+app.get('/api/fairytales/filterByAge/:age', (req, res) => {
+  const requestedAge = req.params.age.toLowerCase();
+  let filteredTales = [];
+
+  if (requestedAge === 'all') {
+    filteredTales = fairyTales;
+  } else {
+    filteredTales = fairyTales.filter(tale => {
+      const taleAge = tale.age.toLowerCase();
+      if (taleAge === requestedAge) {
+        return true;
+      } else {
+        const [min, max] = requestedAge.split('-').map(num => parseInt(num));
+        const [taleMin, taleMax] = taleAge.split('-').map(num => parseInt(num));
+        return taleMin >= min && taleMax <= max;
+      }
+    });
+  }
+
+  if (filteredTales.length === 0) {
+    res.status(404).json({ message: 'No fairy tales found for the specified age range' });
+  } else {
+    res.json(filteredTales);
+  }
+});
+// Route to sort fairy tales by title (ascending and descending)
+app.get("/api/fairytales/sortByTitle/:order", (req, res) => {
+  const order = req.params.order.toLowerCase();
+  let sortedTales = [];
+  if (order === "asc") {
+    sortedTales = fairyTales.sort((a, b) => a.title.localeCompare(b.title));
+  } else if (order === "desc") {
+    sortedTales = fairyTales.sort((a, b) => b.title.localeCompare(a.title));
+  } else {
+    return res.status(400).json({ message: "Invalid sorting order" });
+  }
+  res.json(sortedTales);
+});
+
+// Route to sort fairy tales by created_at (ascending and descending)
+app.get("/api/fairytales/sortByCreatedAt/:order", (req, res) => {
+  const order = req.params.order.toLowerCase();
+  let sortedTales = [];
+  if (order === "asc") {
+    sortedTales = fairyTales.sort(
+      (a, b) => new Date(a.created_at) - new Date(b.created_at)
+    );
+  } else if (order === "desc") {
+    sortedTales = fairyTales.sort(
+      (a, b) => new Date(b.created_at) - new Date(a.created_at)
+    );
+  } else {
+    return res.status(400).json({ message: "Invalid sorting order" });
+  }
+  res.json(sortedTales);
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
