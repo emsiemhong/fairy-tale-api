9a68ff710d4e4ef9975218de1d554aad117819f3
sopheakmen-pse <sopheak.men@pse.ngo>
2024-03-27 13:30:09 +0700

    Update readme

--- api/index.js
+++ api/index.js
@@ -85,7 +85,7 @@ app.get("/api/fairytales/sortByCreatedAt/:order", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
